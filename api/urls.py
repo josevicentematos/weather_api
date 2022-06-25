98a1022fd93ba03fff65902b0c89521c1e53a740
Jose Matos <josevicenteramonez@gmail.com>
2022-06-25 15:25:00 -0300

    added url pattern that accepts query strings

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('weather/<str:city>/<str:country>',
          views.get_weather, name="weather"),
+
+    path('weather',
+         views.get_weather_query_string, name="weather_query_string"),
 ]
